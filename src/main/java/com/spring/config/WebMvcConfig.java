7f7423841b49e15100310e99bb5063c2ea3af32b
DESKTOP-36IPIBF\Faiz <faiz.krm@gmail.com>
2018-08-12 10:04:21 +0530

    Revert "quartz with spring example"
    This reverts commit e69c4400e3d4d85953848c1fd6d3750c09e482ed.

--- src/main/java/com/spring/config/WebMvcConfig.java
+++ src/main/java/com/spring/config/WebMvcConfig.java
@@ -1,8 +1,10 @@
 package com.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.spring.util.common.CommonConstants;

3f5e185a069b1bc5ac2103dd044cc3a07f9bced3
Faiz Akram <faiz.akram@compunnel.in>
2019-02-18 12:39:18 +0530

    commit

--- src/main/java/com/spring/config/WebMvcConfig.java
+++ src/main/java/com/spring/config/WebMvcConfig.java
@@ -1,8 +1,12 @@
 package com.spring.config;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -10,16 +14,28 @@ import com.spring.util.common.CommonConstants;
 
 @Configuration
 @EnableWebMvc
+@EnableAsync
 @ComponentScan(basePackages = { CommonConstants.BASE_PACKAGE })
 public class WebMvcConfig {
 
-		// CommonsMultipartResolver
-		@Bean
-		public CommonsMultipartResolver multipartResolver() {
-			CommonsMultipartResolver resolver = new CommonsMultipartResolver();
-			//resolver.setMaxInMemorySize(50000000);
-			resolver.setDefaultEncoding("utf-8");
-			return resolver;
-		}
-	
+	// CommonsMultipartResolver
+	/*@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		// resolver.setMaxInMemorySize(50000000);
+		resolver.setDefaultEncoding("utf-8");
+		return resolver;
+	}
+*/
+	@Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsynchThread-");
+        executor.initialize();
+        return executor;
+    }
+
 }
